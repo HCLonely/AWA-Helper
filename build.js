bccadea361cec09399b320a67e8a692473447fb3
HCLonely <h1606051253@gmail.com>
2022-04-20 09:57:47 +0800

    修复版本号显示错误

--- build.js
+++ build.js
@@ -2,11 +2,11 @@
   const fs = require('fs-extra');
   const zipdir = require('zip-dir');
 
-  fs.writeFileSync('dist/index.js', fs.readFileSync('dist/index.js').toString().replace('__VERSION__', fs.readJSONSync('package.json').version));
-
-  if (!process.argv.includes('--test')) {
-    fs.copyFileSync('config.example.yml', 'output/config.example.yml');
-    fs.writeFileSync('output/运行.bat', 'start cmd /k "AWA-Helper.exe"');
-    await zipdir('output', { saveTo: './AWA-Helper.zip' });
+  if (process.argv.includes('--pre')) {
+    fs.writeFileSync('dist/index.js', fs.readFileSync('dist/index.js').toString().replace('__VERSION__', fs.readJSONSync('package.json').version));
+    return;
   }
+  fs.copyFileSync('config.example.yml', 'output/config.example.yml');
+  fs.writeFileSync('output/运行.bat', 'start cmd /k "AWA-Helper.exe"');
+  await zipdir('output', { saveTo: './AWA-Helper.zip' });
 })();
